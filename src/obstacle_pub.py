8db4df6fab10e04e5faac3f5212a5a7b4d67fa83
Levi D <devries@usna.edu>
2021-03-08 09:25:45 -0500

    adding obstacle avoidance

---
+++ src/obstacle_pub.py
@@ -0,0 +1,79 @@
+#! /usr/bin/env python3
+
+import rospy
+import math
+import numpy as np
+
+from ackermann_msgs.msg import *
+from std_msgs.msg import Empty, String, Header, Float64
+from nav_msgs.msg import Odometry, Path
+from geometry_msgs.msg import Quaternion, Pose, Polygon, Point32
+#from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import tf
+#from tf_conversions import quaternion_to_euler
+
+# exectutes pose controller from atsushi code and Corker Robotics book
+
+
+class ObstaclePublisher:
+    def __init__(self):
+
+        self.detection_threshold = rospy.get_param('detection_distance_threshold',1.5) # default to 1.5 m
+        self.fov_threshold = rospy.get_param('FOV',math.pi/2)
+        self.obstacle_pub = rospy.Publisher('obstacle',Polygon,queue_size=5)
+        self.qtm_sub = rospy.Subscriber('odom',Odometry,self.odomCallBack)
+        self.send_timer = rospy.Timer(rospy.Duration(0.1), self.sendCallBack)
+        self.ob_list_g = np.array([[1,6],[0,1.5]])
+        self.ob_list_b = np.zeros([2,3])
+        self.dist = 0.0
+        self.xpos = 0.0
+        self.ypos = 3
+
+    def odomCallBack(self,msg):
+        orientation_q = msg.pose.pose.orientation # extract quaternion from pose message
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w] # put quaternion elements into an array
+        euler = tf.transformations.euler_from_quaternion(orientation_list)
+        self.yaw = euler[2]
+
+        #self.speed = math.sqrt(speedx*speedx + speedy*speedy)
+        self.xpos = msg.pose.pose.position.x # parse x-component of position from odometry message
+        self.ypos = msg.pose.pose.position.y # parse y-component of position from odometry message
+
+        # need to calculate relative positiion vector, then rotate
+        for i in range(self.ob_list_g.shape[0]):
+            xrel = self.ob_list_g[i,0]-self.xpos
+            yrel = self.ob_list_g[i,1]-self.ypos
+            self.ob_list_b[i,0] = math.cos(self.yaw)*xrel + math.sin(self.yaw)*yrel # x-position in body fixed frame
+            self.ob_list_b[i,1] = -math.sin(self.yaw)*xrel + math.cos(self.yaw)*yrel # y-position in body fixed frame
+            self.ob_list_b[i,2] = math.atan2(self.ob_list_b[i,1],self.ob_list_b[i,0]) # angle to obstacle center in body-fixed frame
+            # also need to check if point is in FOV of car
+
+    def sendCallBack(self,msg):
+        tmp = Polygon()
+        count = 0
+        for i in range(self.ob_list_b.shape[0]):
+            # calculate how far I am from waypoint
+            dist = math.sqrt((self.ob_list_b[i,0])**2 + (self.ob_list_b[i,1])**2)
+            #rospy.loginfo("%f,%f",i,dist)
+            if (dist<=self.detection_threshold and abs(self.ob_list_b[i,2]) <= self.fov_threshold):
+                tmp2 = Point32()
+                # package polygon message
+                tmp2.x = self.ob_list_b[i,0]
+                tmp2.y = self.ob_list_b[i,1]
+                tmp.points.append(tmp2)
+                count += 1
+        #rospy.loginfo(count)
+        if count>0:
+            # publish point message
+            rospy.loginfo("Publishing %f obstacle(s)",count)
+            self.obstacle_pub.publish(tmp)
+
+
+
+
+if __name__ == '__main__':
+    rospy.init_node('obstacle_publisher')
+
+    Obstacles = ObstaclePublisher()
+
+    rospy.spin()
---
+++ src/pose_controller.py
@@ -0,0 +1,200 @@
+#! /usr/bin/env python3
+
+import rospy
+import math
+import numpy as np
+
+from ackermann_msgs.msg import *
+from std_msgs.msg import Empty, String, Header, Float64
+from nav_msgs.msg import Odometry, Path
+from geometry_msgs.msg import Twist, Quaternion, Pose
+#from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import tf
+#from tf_conversions import quaternion_to_euler
+
+# exectutes pose controller from atsushi code and Corker Robotics book
+
+
+class roverWaypointControl:
+    def __init__(self):
+
+        self.max_str_angle = rospy.get_param('max_str_angle',0.75)
+        self.max_thr = rospy.get_param('max_throttle',0.2)
+        self.desSpeed = rospy.get_param('desired_speed',1)
+        self.dist_threshold = rospy.get_param('waypoint_distance_threshold',0.4) # default to 10 cm
+        self.L = rospy.get_param('wheel_base',0.2) # 0.2 meters
+        self.KpSpd = rospy.get_param('Kp_spd',1.0)
+        self.KiSpd = rospy.get_param('Ki_spd',0.05)
+        self.KpYaw = rospy.get_param('Kp_yaw',3.5)
+
+        self.acker_pub = rospy.Publisher('acker_cmd',AckermannDriveStamped,queue_size=5)
+        self.wp_sub = rospy.Subscriber('waypoint',Pose,self.wptCallBack)
+        self.path_sub = rospy.Subscriber('path',Path,self.pathCallBack)
+        self.qtm_sub = rospy.Subscriber('odom',Odometry,self.odomCallBack)
+        self.send_timer = rospy.Timer(rospy.Duration(0.1), self.sendCallBack)
+
+        self.ackMsg = AckermannDriveStamped()
+        self.twistMsg = Twist()
+        self.yawRate = 0.0 # u2 variable
+        self.xdes = 2.0
+        self.ydes = 2.0
+        self.xpos = 0.0
+        self.ypos = 0.0
+        self.xerr = 0.0
+        self.yerr = 0.0
+        self.yaw = 0.0
+        self.speed = 0.0
+        self.dist = 0.0
+        self.spdErrInt = 0
+        self.wp_list = []
+        self.wp_n = 0
+        self.wp_ind = 0
+        self.yaw_des = 0
+        self.xvel_b = 0
+        self.yvel_b = 0
+        self.xvel_g = 0
+        self.yvel_g = 0
+        self.mission = False
+        self.my_path = Path()
+
+    def wptCallBack(self,msg):
+        self.xdes = msg.position.x
+        self.ydes = msg.position.y
+        self.mission = True
+
+    def pathCallBack(self,msg):
+
+        print("%d",len(msg.poses))
+        self.wp_n = len(msg.poses)
+        self.wp_list = np.zeros([3,self.wp_n])
+        for i in range(self.wp_n):
+            self.wp_list[0,i] = msg.poses[i].pose.position.x
+            self.wp_list[1,i] = msg.poses[i].pose.position.y
+            orientation_q = msg.poses[i].pose.orientation # extract quaternion from pose message
+            orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w] # put quaternion elements into an array
+            euler = tf.transformations.euler_from_quaternion(orientation_list)
+            rospy.loginfo(euler)
+            self.wp_list[2,i] = euler[2]
+
+        print(self.wp_list)
+        self.mission = True
+
+        #self.xdes = msg.position.x # Modify for path
+        #self.ydes = msg.position.y
+
+    def odomCallBack(self,msg):
+        orientation_q = msg.pose.pose.orientation # extract quaternion from pose message
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w] # put quaternion elements into an array
+        euler = tf.transformations.euler_from_quaternion(orientation_list)
+        self.yaw = euler[2]
+
+        #(roll, pitch, yaw) = euler_from_quaternion (orientation_list) # convert quaternion to Euler angles
+        self.xvel_g = msg.twist.twist.linear.x # parse x-component of inertial speed from odometry message
+        self.yvel_g = msg.twist.twist.linear.y # parse y-component of inertial speed from odometry message
+
+        self.xvel_b = math.cos(self.yaw)*self.xvel_g + math.sin(self.yaw)*self.yvel_g
+        self.yvel_b = -math.sin(self.yaw)*self.xvel_g + math.cos(self.yaw)*self.yvel_g
+
+        #self.speed = math.sqrt(speedx*speedx + speedy*speedy)
+        self.xpos = msg.pose.pose.position.x # parse x-component of position from odometry message
+        self.ypos = msg.pose.pose.position.y # parse y-component of position from odometry message
+
+    def sendCallBack(self,msg):
+        # Steering controller (using yaw rate speed kinematics)
+        q = [0,0,0,1]
+        euler = tf.transformations.euler_from_quaternion(q)
+        if(self.mission):
+            steerAng = Float64()
+
+            if(self.wp_ind == self.wp_n): # If we reached the last waypoint start over
+                self.wp_ind = 0
+
+            self.xdes = self.wp_list[0,self.wp_ind]
+            self.ydes = self.wp_list[1,self.wp_ind]
+            goal_ang = self.wp_list[2,self.wp_ind]
+
+            self.xerr = self.xdes-self.xpos
+            self.yerr = self.ydes-self.ypos
+
+            #self.dist = math.sqrt(x_err*x_err + y_err*y_err)
+            # calculate how far I am from waypoint
+            self.dist = math.sqrt((self.xerr)**2 + (self.yerr)**2)
+
+            if(self.dist < self.dist_threshold):
+                self.wp_ind = self.wp_ind + 1
+
+
+            self.yaw_des = math.atan2(self.yerr,self.xerr)
+            yaw_position = (self.yaw_des-self.yaw) # alpha term in other code
+
+            if yaw_position>math.pi:
+                yaw_position = yaw_position-2*math.pi
+            elif yaw_position<-math.pi:
+                yaw_position = yaw_position+2*math.pi
+
+            yaw_goal_beta = goal_ang-self.yaw-yaw_position
+
+            if yaw_goal_beta>math.pi:
+                yaw_goal_beta = yaw_goal_beta-2*math.pi
+            elif yaw_goal_beta<-math.pi:
+                yaw_goal_beta = yaw_goal_beta+2*math.pi
+
+            self.yawRate = self.KpYaw*yaw_position - 0.2*self.KpYaw*yaw_goal_beta
+            if abs(self.xvel_b)<0.1: # if speed less than 10 cm/s, use speed invariant turn angle
+                steerAng = math.atan(self.yawRate*self.L/0.1)
+            else: # if speed greater than 10 cm/s, use speed dependent steering angle
+                steerAng = math.atan(self.yawRate*self.L/self.xvel_b)
+
+            if abs(steerAng)>self.max_str_angle:
+                steerAng = math.copysign(self.max_str_angle,steerAng)
+
+
+
+            # if vehicle is within distance threshold, stop
+            #if self.dist<self.dist_threshold:
+             #  self.thr_cmd = 0.0 # stop if within threshold of waypoint
+
+
+            # speed controller (Using PI controller on acceleration)
+            self.spdErr = self.desSpeed - self.xvel_b
+
+            # update speed error integral term
+            self.spdErrInt = self.spdErrInt + self.spdErr*0.1 # 10 Hz sampling rate
+            # saturate speed error integral term at 2
+            #if self.spdErrInt > 2:
+            #    self.spdErrInt = 2.0
+            if(self.spdErrInt > 0.05):
+                self.spdErrInt = 0.05
+            elif (self.spdErrInt < -0.05):
+                self.spdErrInt = -0.05
+            else:
+                self.spdErrInt = self.spdErrInt;
+
+            # PWM duty cycle for throttle control
+            self.thr_cmd = self.KpSpd*self.spdErr + self.KiSpd*self.spdErrInt # PI controller
+
+            # Saturate speed command at 20% duty cycle
+            if self.thr_cmd > self.max_thr:
+                self.thr_cmd = self.max_thr
+
+            if self.thr_cmd < -self.max_thr:
+                self.thr_cmd = -self.max_thr
+
+
+            # package ackermann message
+            self.ackMsg.drive.steering_angle = steerAng
+            self.ackMsg.drive.acceleration = self.thr_cmd
+
+            rospy.loginfo("%f,%f,%f,%f",self.xdes,self.ydes,self.xpos,self.ypos)
+            # publish ackermann message
+            self.acker_pub.publish(self.ackMsg)
+        else:
+            rospy.loginfo("Waiting for Mission")
+
+
+if __name__ == '__main__':
+    rospy.init_node('waypoint_navigation')
+
+    myRover = roverWaypointControl()
+
+    rospy.spin()
